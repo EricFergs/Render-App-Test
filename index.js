b5f2bc0031f73114363eb2d59c51f754919cadb4
EricFergs <135307549+EricFergs@users.noreply.github.com>
2025-03-19 23:44:20 -0700

    Eslint

--- index.js
+++ index.js
@@ -2,14 +2,14 @@ const express = require('express')
 const morgan = require('morgan')
 const app = express()
 const Phone = require('./models/phone')
-require('dotenv').config();
+require('dotenv').config()
 
 const requestLogger = (request,response,next) => {
-    console.log('Method:', request.method)
-    console.log('Path: ', request.path)
-    console.log('Body: ',request.body)
-    console.log('---')
-    next()
+  console.log('Method:', request.method)
+  console.log('Path: ', request.path)
+  console.log('Body: ',request.body)
+  console.log('---')
+  next()
 }
 
 
@@ -21,18 +21,18 @@ app.use(morgan(':method :url :status :response-time ms :res[content-length] byte
 
 
 const unknownEndpoint = (request,response) => {
-    response.status(404).send({error: 'unknown endpoint'})
+  response.status(404).send({ error: 'unknown endpoint' })
 }
 
 const errorHandler = (error, request, response, next) => {
-    console.log(error.message)
-    if (error.name === 'CastError') {
-        return response.status(400).send({error: 'malformatted id'})
-    } else if (error.name === 'ValidationError') {
-        return response.status(400).json({error: error.message})
-    }
+  console.log(error.message)
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } else if (error.name === 'ValidationError') {
+    return response.status(400).json({ error: error.message })
+  }
 
-    next(error)
+  next(error)
 }
 // app.use(requestLogger)
 
@@ -40,73 +40,73 @@ const errorHandler = (error, request, response, next) => {
 
 
 app.get('/api/persons',(request,response) => {
-    Phone.find({}).then(result => {
-        response.json(result)
-    })
+  Phone.find({}).then(result => {
+    response.json(result)
+  })
 })
 
 
 app.get('/api/persons/:id',(request,response,next) => {
-    const id = request.params.id
-    Phone.findById(id).then(phone => {
-        if (phone){
-            return response.json(phone)
-        }
-        else{
-            return response.status(404).end()
-        }
-    }).catch(error => next(error))
+  const id = request.params.id
+  Phone.findById(id).then(phone => {
+    if (phone){
+      return response.json(phone)
+    }
+    else{
+      return response.status(404).end()
+    }
+  }).catch(error => next(error))
 })
 
 app.delete('/api/persons/:id',(request,response,next) => {
-    const id = request.params.id
-    Phone.findByIdAndDelete(id)
-        .then(result => {
-            response.status(204).end()
-        })
-        .catch(error => next(error))
+  const id = request.params.id
+  Phone.findByIdAndDelete(id)
+    .then(result => {
+      response.status(204).end()
+    })
+    .catch(error => next(error))
 })
 
 app.put('/api/persons',(request,response, next) => {
-    const {name, number, id} = request.body
-    if(!name || !number){
-        return response.status(404).json({
-            error : "Mising body or number"
-        })
-    }
-    Phone.findById(id)
-        .then(phone => {
-            if (!phone) {
-                return response.status(404).json({error: "The person does not exists anymore"})
-            }
+  const { name, number, id } = request.body
+  if(!name || !number){
+    return response.status(404).json({
+      error : 'Mising body or number'
+    })
+  }
+  Phone.findById(id)
+    .then(phone => {
+      if (!phone) {
+        return response.status(404).json({ error: 'The person does not exists anymore' })
+      }
 
-            phone.name = name
-            phone.number = number
+      phone.name = name
+      phone.number = number
 
-            return phone.save().then((updatedPhone => {
-                response.json(updatedPhone)
-            }))
-        })
-        .catch(error => next(error))
+      return phone.save().then((updatedPhone => {
+        response.json(updatedPhone)
+      }))
+    })
+    .catch(error => next(error))
 })
 
 app.post('/api/persons',(request,response,next) => {
-    const body = request.body
-   
-    Phone.findOne({name: body.name}).then(exists => {
-        if (exists){
-            return response.status(409).json({error : "Already exists"})
-        }
-        const phone = new Phone({
-            "name" : body.name,
-            "number": body.number
-        })
-        phone.save().then(savedPhone => {
-            return response.json(savedPhone)
-        })
-        .catch(error=>next(error))
+  const body = request.body
+
+  Phone.findOne({ name: body.name }).then(exists => {
+    if (exists){
+      return response.status(409).json({ error : 'Already exists' })
+    }
+    const phone = new Phone({
+      'name' : body.name,
+      'number': body.number
     })
-    
+    phone.save().then(savedPhone => {
+      return response.json(savedPhone)
+    })
+      .catch(error => next(error))
+  })
+
 })
 
 
@@ -117,5 +117,5 @@ app.use(errorHandler)
 
 const PORT =  process.env.PORT || 3001
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`)
+  console.log(`Server running on port ${PORT}`)
 })

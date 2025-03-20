1c667dd988656511079b3a573156353e41544e44
EricFergs <135307549+EricFergs@users.noreply.github.com>
2025-03-19 22:22:07 -0700

    Validation and Database Added

--- index.js
+++ index.js
@@ -1,14 +1,8 @@
 const express = require('express')
 const morgan = require('morgan')
 const app = express()
-
-
-
-app.use(express.json())
-app.use(express.static('dist'))
-morgan.token('body',(req) => JSON.stringify(req.body) || '{}')
-app.use(morgan(':method :url :status :response-time ms :res[content-length] bytes :body'))
-
+const Phone = require('./models/phone')
+require('dotenv').config();
 
 const requestLogger = (request,response,next) => {
     console.log('Method:', request.method)
@@ -17,8 +11,28 @@ const requestLogger = (request,response,next) => {
     console.log('---')
     next()
 }
+
+
+app.use(express.json())
+// app.use(requestLogger)
+app.use(express.static('dist'))
+morgan.token('body',(req) => JSON.stringify(req.body) || '{}')
+app.use(morgan(':method :url :status :response-time ms :res[content-length] bytes :body'))
+
+
 const unknownEndpoint = (request,response) => {
     response.status(404).send({error: 'unknown endpoint'})
+}
+
+const errorHandler = (error, request, response, next) => {
+    console.log(error.message)
+    if (error.name === 'CastError') {
+        return response.status(400).send({error: 'malformatted id'})
+    } else if (error.name === 'ValidationError') {
+        return response.status(400).json({error: error.message})
+    }
+
+    next(error)
 }
 // app.use(requestLogger)
 
@@ -47,7 +61,9 @@ data = [
 ]
 
 app.get('/api/persons',(request,response) => {
-    response.json(data)
+    Phone.find({}).then(result => {
+        response.json(result)
+    })
 })
 
 app.get('/info',(request,response) => {
@@ -56,67 +72,74 @@ app.get('/info',(request,response) => {
     response.send(`<p>The phone book has ${length} many people <br/><br/> ${dateTime}</p>`)
 })
 
-app.get('/api/persons/:id',(request,response) => {
+app.get('/api/persons/:id',(request,response,next) => {
     const id = request.params.id
-    const person = data.find(p => p.id === id)
-    if (person){
-        return response.json(person)
-    }
-    return response.status(404).end()
+    Phone.findById(id).then(phone => {
+        if (phone){
+            return response.json(phone)
+        }
+        else{
+            return response.status(404).end()
+        }
+    }).catch(error => next(error))
 })
 
-app.delete('/api/persons/:id',(request,response) => {
+app.delete('/api/persons/:id',(request,response,next) => {
     const id = request.params.id
-    data = data.filter(p => p.id !== id)
-    response.status(204).end()
-})
-
-app.put('/api/persons',(request,response) => {
-    const body = request.body
-    if(!body.number || !body.id){
-        return response.status(404).json({
-            error : "Missing body"
+    Phone.findByIdAndDelete(id)
+        .then(result => {
+            response.status(204).end()
         })
-    }
-    const id = body.id
-    const person = data.find(p => p.id === id)
-    if (person){
-        person.number = body.number
-        data.map(p => p.id === id ? person : p)
-        return response.json(person)
-    }
-    return response.status(404).json({error : "Person not found"})
+        .catch(error => next(error))
 })
 
-const randomNumber = () => {
-    return String(Math.floor(Math.random() * 10000) + 1)
-}
-app.post('/api/persons',(request,response) => {
-    const body = request.body
-    if(!body.name || !body.number){
+app.put('/api/persons',(request,response, next) => {
+    const {name, number, id} = request.body
+    if(!name || !number){
         return response.status(404).json({
             error : "Mising body or number"
         })
     }
-    const person = data.find(p => p.name === body.name)
+    Phone.findById(id)
+        .then(phone => {
+            if (!phone) {
+                return response.status(404).json({error: "The person does not exists anymore"})
+            }
 
-    if(person){ 
-        return response.status(404).json({
-            error: "Person already exists"
+            phone.name = name
+            phone.number = number
+
+            return phone.save().then((updatedPhone => {
+                response.json(updatedPhone)
+            }))
         })
-    }
-    const newPerson = {
-        "id" : randomNumber(),
-        "name" : body.name,
-        "number": body.number
-    }
-    data = data.concat(newPerson)
-    return response.json(newPerson)
+        .catch(error => next(error))
+})
+
+app.post('/api/persons',(request,response,next) => {
+    const body = request.body
+   
+    Phone.findOne({name: body.name}).then(exists => {
+        if (exists){
+            return response.status(409).json({error : "Already exists"})
+        }
+        const phone = new Phone({
+            "name" : body.name,
+            "number": body.number
+        })
+        phone.save().then(savedPhone => {
+            return response.json(savedPhone)
+        })
+        .catch(error=>next(error))
+    })
+    
 })
 
 
 
 app.use(unknownEndpoint)
+
+app.use(errorHandler)
 
 const PORT =  process.env.PORT || 3001
 app.listen(PORT, () => {
---
+++ models/phone.js
@@ -0,0 +1,62 @@
+const mongoose = require('mongoose')
+require('dotenv').config()
+
+mongoose.set('strictQuery',false)
+
+const url = process.env.MONGODB_URI
+
+console.log("Connecting to URL")
+mongoose.connect(url)
+    .then(result => {
+        console.log('conncting to MongoDB')
+    })
+    .catch(error => {
+        console.log("Error connecting to my beloved url", error.message)
+    })
+    
+
+const phoneSchema = new mongoose.Schema({
+    id: Number,
+    name: {
+        type: String,
+        minLength: 3,
+        required: true
+    },
+    number: {
+        type: String,
+        minLength: 8,
+        required: true,
+        validate:{ 
+            validator : function(value) {
+                hasDash = false
+                for(let i = 0; i < value.length; i ++){
+                    console.log(value[i])
+                    if ((i == 2 || i == 3) && value[i] === '-'){
+                        hasDash = !hasDash
+                    }
+                    else if (isNaN(value[i])) {
+                        return false
+                    }
+                }
+                if (hasDash){
+                    return true
+                }
+                return false
+                },
+            message: props => `${props.value} is not valid phone number`
+        }
+        
+    }
+
+})
+
+phoneSchema.set('toJSON', {
+    transform: (document, returnedObject) => {
+      returnedObject.id = returnedObject._id.toString()
+      delete returnedObject._id
+      delete returnedObject.__v
+    }
+})
+
+module.exports = mongoose.model("Phone",phoneSchema)
+
